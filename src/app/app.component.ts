c4b276af2b7da292ce2ccc782db7dc694d9a081d
Dmast00 <obed.hinojosa@outlook.com>
2022-07-11 10:41:21 -0500

    Se agrego el avatar random

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ScheduleEmailsFrontEnd';
+  name  = localStorage.getItem('userName')
+  number = Math.floor(Math.random() *5)+1;  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MatCardModule } from "@angular/material/card";
 
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
